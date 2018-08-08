6553afda8f8035f7553c1bfc64b29038b2a5ec57
Jeu Casulo <jeucasulo@hotmail.com>
2018-08-07 22:46:05 -0300

    Passport install

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use Laravel\Passport\Passport;
+
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes();
 
         //
     }
